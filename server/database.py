443a316afb55f50fb19acba4ceb08edb8ef2fd4f
cshackerpro <vcm@vcm-2400.vm.duke.edu>
2017-12-12 16:00:35 -0500

    Fixed database user

--- server/database.py
+++ server/database.py
@@ -1,22 +1,25 @@
 import datetime
 from pymongo import MongoClient
-from pymodm import MongoModel, fields
+from pymodm import MongoModel, fields, connect
 
 client = MongoClient("mongodb://vcm-1915.vm.duke.edu:27017")
 db = client.bm590
+connect("mongodb://vcm-1915.vm.duke.edu:27017/bme590")
 
 class User(MongoModel):
     patient_name   = fields.CharField()
     user_id        = fields.CharField()
-    object_id      = fields.ObjectIdField()
+    #object_id      = fields.ObjectIdField()
     password       = fields.CharField()
-    dimensions     = fields.IntegerField()
+    dimension1     = fields.IntegerField()
+    dimension2     = fields.IntegerField()
     classification = fields.BooleanField()
 
 def insert(content):
     content = {"image_data": content,
                "date": datetime.datetime.utcnow()}
-    results = database.images.insert_one(content)
+    result = db.images.insert_one(content)
+    User('Testname','345','password', '1080', '2400','True').save()
     return result.inserted_id
 
 if __name__ == '__main__':
