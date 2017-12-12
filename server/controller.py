075cd2bdade8a5eec998474c2e689f98e47e8c4f
cshackerpro <brianna.loomis@duke.edu>
2017-12-12 14:47:20 -0500

    Testing tensorflow additions

--- server/controller.py
+++ server/controller.py
@@ -1,6 +1,16 @@
 import db
+from tensorflow_for_poets.scripts import label_image
 
 def store_image(content):
     image_index = db(content)
     # image_index = 1;
     return image_index
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
